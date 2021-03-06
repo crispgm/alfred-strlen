6e289ce6328c2f37070c21ff7b39c10a2e0ebb64
David Zhang <crispgm@gmail.com>
2021-03-06 21:21:52 +0800

    fix: careless issue

--- alfred_main_test.go
+++ alfred_main_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 func TestLength(t *testing.T) {
-	assert.Equal(t, 12, lengthOf("hello, world", lenByRunes))
-	assert.Equal(t, 3, lengthOf("123", lenByRunes))
-	assert.Equal(t, 4, lengthOf("你好世界", lenByRunes))
+	assert.Equal(t, 12, lengthOf("hello, world", lenByRune))
+	assert.Equal(t, 3, lengthOf("123", lenByRune))
+	assert.Equal(t, 4, lengthOf("你好世界", lenByRune))
 
 	assert.Equal(t, 12, lengthOf("hello, world", lenBySize))
 	assert.Equal(t, 3, lengthOf("123", lenBySize))
